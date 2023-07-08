cf7848fcfa774a28b1a7bf6fdac1d28e19f8c4a7
edeermachado <edeermachado@hotmail.com>
2023-07-08 18:54:57 -0300

    Add Any types @ setAndForwardRef.ts

--- src/setAndForwardRef.ts
+++ src/setAndForwardRef.ts
@@ -1,16 +1,74 @@
-export function setAndForwardRef({ getForwardedRef, setLocalRef }) {
-    return function forwardRef(ref) {
-        const forwardedRef = getForwardedRef();
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ * @flow
+ */
 
-        setLocalRef(ref);
+'use strict';
 
-        // Forward to user ref prop (if one has been specified)
-        if (typeof forwardedRef === "function") {
-            // Handle function-based refs. String-based refs are handled as functions.
-            forwardedRef(ref);
-        } else if (typeof forwardedRef === "object" && forwardedRef != null) {
-            // Handle createRef-based refs
-            forwardedRef.current = ref;
-        }
-    };
+import type { ElementRef, Ref } from 'react';
+
+// type Args = $ReadOnly<{|
+//   getForwardedRef: () => ?Ref<any>,
+//   setLocalRef: (ref: ElementRef<any>) => mixed,
+// |}>;
+
+type Args = {
+  // getForwardedRef: () => Ref<any>;
+  getForwardedRef: () => any;
+  setLocalRef: (ref: ElementRef<any>) => any;
 }
+
+/**
+ * This is a helper function for when a component needs to be able to forward a ref
+ * to a child component, but still needs to have access to that component as part of
+ * its implementation.
+ *
+ * Its main use case is in wrappers for native components.
+ *
+ * Usage:
+ *
+ *   class MyView extends React.Component {
+ *     _nativeRef = null;
+ *
+ *     _setNativeRef = setAndForwardRef({
+ *       getForwardedRef: () => this.props.forwardedRef,
+ *       setLocalRef: ref => {
+ *         this._nativeRef = ref;
+ *       },
+ *     });
+ *
+ *     render() {
+ *       return <View ref={this._setNativeRef} />;
+ *     }
+ *   }
+ *
+ *   const MyViewWithRef = React.forwardRef((props, ref) => (
+ *     <MyView {...props} forwardedRef={ref} />
+ *   ));
+ *
+ *   module.exports = MyViewWithRef;
+ */
+
+export default function setAndForwardRef({ getForwardedRef, setLocalRef }: Args): (ref: ElementRef<any>) => void {
+  return function forwardRef(ref: ElementRef<any>) {
+    const forwardedRef = getForwardedRef();
+
+    setLocalRef(ref);
+
+    // Forward to user ref prop (if one has been specified)
+    if (typeof forwardedRef === 'function') {
+      // Handle function-based refs. String-based refs are handled as functions.
+      forwardedRef(ref);
+    } else if (typeof forwardedRef === 'object' && forwardedRef != null) {
+      // Handle createRef-based refs
+      forwardedRef.current = ref;
+    }
+  };
+}
+
+export { setAndForwardRef };
